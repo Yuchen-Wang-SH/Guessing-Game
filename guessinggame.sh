6779e5bec1b676a16444536e5c63e8c6751d5ebe
YuChen Wang <569816003@qq.com>
2019-02-16 09:46:51 +0800

    Made program more robust.

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 echo "How many files are in this directory?"
-numfiles=$(ls | wc -l)
+numfiles=$(find . -maxdepth 1 -type f | wc -l)
 
 function guess {
     while true
     do
         read answer
-        if [[ $answer -gt $numfiles ]]
+        if ! [[ $answer =~ ^-?[0-9]+$ ]]
         then
-            echo "Your answer is too big!"
+            echo "Your answer is not a valid integer. Please try again."
+        elif [[ $answer -gt $numfiles ]]
+        then
+            echo "Your answer is too big! Please try again."
         elif [[ $answer -lt $numfiles ]]
         then
-            echo "Your answer is too small!"
+            echo "Your answer is too small! Please try again."
         else
             echo "You have the right answer! Congratulations!"
             break
